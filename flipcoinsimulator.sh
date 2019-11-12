0b1dfdcfdd563041d517036380b1c6b7ddd317ed
luckyNB <20150773@dbatu.ac.in>
2019-11-12 14:59:04 +0530

    flip coin percentage

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,10 +1,30 @@
 #!/bin/bash -x
-head=0
+head=1
 tail=0
-ResultCheck=$((RANDOM %2))
-if [ $ResultCheck -eq 0 ]
- then 
- echo "head"
-else 
- echo "tail"
-fi
+countHead=0
+countTail=0
+
+declare -A coins
+echo  "enter noOf time flipcoin"
+read flipcoin
+noOfTime=$flipcoin
+while  [ $flipcoin -gt 0 ]
+ do
+ ResultCheck=$((RANDOM %2))
+
+   if [ $ResultCheck -eq 1 ]
+      then 
+      coins[$((countHead++))]="Head"
+
+  else 
+    coins[$((countTail++))]="Tail"
+    
+  fi
+flipcoin=$(($flipcoin - 1))
+done
+
+headPer=$(($countHead*100/$noOfTime))
+tailPer=$(($countTail*100/$noOfTime))
+echo ${coins[@]}
+
+echo "headPer :$headPer ,tailper :$tailPer"
